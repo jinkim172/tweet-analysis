d1c024858a2eda10ee9cb85beec3b87b05021c96
Jin U Kim <jinukim@MBP-2.local>
2020-02-26 12:53:51 -0500

    Changes to make collector stream and search.

--- get_tweets.py
+++ get_tweets.py
@@ -8,6 +8,8 @@ import json
 auth = tweepy.OAuthHandler('lW1lQkWiSV86ROytPEabdBplB', 'cWUzxdgL2zJokUPRP1Msos00u1HhgI7bp33aLvJ4IkrhbsFjp6') #(consumer_key, consumer_secret)
 auth.set_access_token('1184759057078521856-TYMiapY06vE5cfuhKOOlkeOmWKRMH8', 'BkD0wZcJJYRSg0thaDc7DqW2Cl8NdWtILyGnuJlglWyHy') #(access_token, access_token_secret)
 api = tweepy.API(auth)
+globalnum = 10
+globaltracker = []
 
 class MyStreamListener(tweepy.StreamListener):
     def __init__(self, api=None):
@@ -15,7 +17,7 @@ class MyStreamListener(tweepy.StreamListener):
         self.num_tweets = 0
 
     def on_status(self, status):
-        if self.num_tweets < 5:
+        if self.num_tweets < globalnum:
             text = status.text
             with open("output.txt", "a", encoding='utf-8') as f:
                 f.write(status.text + "\r\n\n")
@@ -26,9 +28,16 @@ class MyStreamListener(tweepy.StreamListener):
             return False
 
     def on_error(self, status):
-        print ('Error on status', status)
+        #print ('Error on status', status)
+        print("Ran out of time on stream API, switching to search API...")
         if status == 420:
             #returning False in on_data disconnects the stream
+            for query in globaltracker:
+                search_results = api.search(q=query, count=globalnum)
+                with open("output.txt", "a", encoding='utf-8') as f:
+                    for i in search_results:
+                        f.write(i.text + "\r\n\n")
+                        print(i.text)
             return False
 
     def on_limit(self, status):
@@ -37,12 +46,10 @@ class MyStreamListener(tweepy.StreamListener):
     def on_timeout(self, status):
         print ('Stream disconnected; continuing...')
 
-#if __name__ == "__main__":
-
 with open("people2.json", "r") as f:
     athlete_dictionary = json.loads(f.read())
 
 for key in athlete_dictionary:
-    tracker = athlete_dictionary[key]
+    globaltracker = athlete_dictionary[key][:]
     stream = tweepy.Stream(auth = api.auth, listener=MyStreamListener())
-    stream.filter(track=tracker)
+    stream.filter(track=globaltracker)
