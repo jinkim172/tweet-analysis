8861b747f643bcaa6401e40842350a442e41dfa1
Jin U Kim <jinukim@MBP-2.local>
2020-02-26 00:43:14 -0500

    Making the tweet collector functional.

--- get_tweets.py
+++ get_tweets.py
@@ -1,9 +1,13 @@
 # get_tweets.py
 # Allen Duong (aduong1), Jin Kim (jkim56), Nicholas Marcopoli (nmarcopo)
-# Description: 
+# Description:
 
 import tweepy
 import json
+
+auth = tweepy.OAuthHandler('lW1lQkWiSV86ROytPEabdBplB', 'cWUzxdgL2zJokUPRP1Msos00u1HhgI7bp33aLvJ4IkrhbsFjp6') #(consumer_key, consumer_secret)
+auth.set_access_token('1184759057078521856-TYMiapY06vE5cfuhKOOlkeOmWKRMH8', 'BkD0wZcJJYRSg0thaDc7DqW2Cl8NdWtILyGnuJlglWyHy') #(access_token, access_token_secret)
+api = tweepy.API(auth)
 
 class MyStreamListener(tweepy.StreamListener):
     def __init__(self, api=None):
@@ -11,19 +15,21 @@ class MyStreamListener(tweepy.StreamListener):
         self.num_tweets = 0
 
     def on_status(self, status):
-            if self.num_tweets < 2:
-                text = status.text
-                # g.write(text + "\r\n\n")
-                with open("out.csv", "a", encoding='utf-8') as f:
-                    f.write("%s\n" % (status.text))
-                print(text)
-                self.num_tweets += 1
-                return True
-            else:
-                return False
+        if self.num_tweets < 5:
+            text = status.text
+            with open("output.txt", "a", encoding='utf-8') as f:
+                f.write(status.text + "\r\n\n")
+            print(text)
+            self.num_tweets += 1
+            return True
+        else:
+            return False
 
     def on_error(self, status):
         print ('Error on status', status)
+        if status == 420:
+            #returning False in on_data disconnects the stream
+            return False
 
     def on_limit(self, status):
         print ('Limit threshold exceeded'), status
@@ -31,21 +37,12 @@ class MyStreamListener(tweepy.StreamListener):
     def on_timeout(self, status):
         print ('Stream disconnected; continuing...')
 
-if __name__ == "__main__":
-    auth = tweepy.OAuthHandler('lW1lQkWiSV86ROytPEabdBplB', 'cWUzxdgL2zJokUPRP1Msos00u1HhgI7bp33aLvJ4IkrhbsFjp6') #(consumer_key, consumer_secret)
-    auth.set_access_token('1184759057078521856-TYMiapY06vE5cfuhKOOlkeOmWKRMH8', 'BkD0wZcJJYRSg0thaDc7DqW2Cl8NdWtILyGnuJlglWyHy') #(access_token, access_token_secret)
-    api = tweepy.API(auth)
-    stream = tweepy.Stream(auth = api.auth, listener=MyStreamListener())
-    
-    # g = open("tweets_storage.txt", "w", encoding='utf-8')
-    
-    # key - athlete name, value = {"handle": "@lebronjames", "affiliation": "NBA", "hashtags": ["#lebronjames", "#kingjames"]}
-    with open("people.json", "r") as f:
-        DICTIONARY_NAME = json.loads(f.read())
+#if __name__ == "__main__":
 
-    for key in DICTIONARY_NAME:
-        tracker=[]
-        tracker.append(DICTIONARY_NAME[key]["twitter_handle"])
-        for hashtag in DICTIONARY_NAME[key]["hashtags"]:
-            tracker.append(hashtag)
-        stream.filter(track=tracker)
+with open("people2.json", "r") as f:
+    athlete_dictionary = json.loads(f.read())
+
+for key in athlete_dictionary:
+    tracker = athlete_dictionary[key]
+    stream = tweepy.Stream(auth = api.auth, listener=MyStreamListener())
+    stream.filter(track=tracker)
