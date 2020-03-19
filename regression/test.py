bf0590f350bbbcf98723bc2f7f685ea4da051901
Nick Marcopoli <nmarcopo@nd.edu>
2020-03-19 04:14:40 +0000

    added initial regression

---
+++ regression/test.py
@@ -0,0 +1,22 @@
+import pandas
+import pickle
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction import DictVectorizer
+from scipy.sparse import hstack
+from sklearn.linear_model import Ridge
+
+data = pandas.read_csv('small_subset_test.csv', header=None)
+
+with open('small_subset_vectorizer.obj', 'rb') as f:
+    vectorizer = pickle.load(f)
+with open('small_subset_model.obj', 'rb') as f:
+    clf = pickle.load(f)
+
+x_test = vectorizer.transform(data.iloc[:, 5].values)
+x = hstack([x_test])
+
+# predict on test data
+results = clf.predict(x)
+
+for tweet, prediction, correct in zip(data.iloc[:, 5].values, results, data.iloc[:, 0].values):
+    print(tweet, "prediction:", prediction, ", correct:", correct)
---
+++ regression/train.py
@@ -0,0 +1,25 @@
+import pandas
+import pickle
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction import DictVectorizer
+from scipy.sparse import hstack
+from sklearn.linear_model import Ridge
+
+data = pandas.read_csv('small_subset_train.csv', header=None)
+
+vectorizer = TfidfVectorizer(min_df=5)
+
+print(len(data.columns))
+x_train = vectorizer.fit_transform(data.iloc[:, 5].values)
+x = hstack([x_train])
+clf = Ridge()
+
+y = data.iloc[:, 0].values
+# fit model to the training data
+clf.fit(x, y)
+
+# save model and vectorizer
+with open('small_subset_model.obj', 'wb') as f:
+    pickle.dump(clf, f)
+with open('small_subset_vectorizer.obj', 'wb') as f:
+    pickle.dump(vectorizer, f)
