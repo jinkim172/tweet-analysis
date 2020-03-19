17e3d3f3b942cfe9204fc6dcde374c4ca05d06aa
Jin Kim <jinkim172@gmail.com>
2020-03-19 19:51:34 -0400

    Number of tweets fix + link quick fix.

--- data_collect.py
+++ data_collect.py
@@ -11,12 +11,11 @@ api = tweepy.API(auth)
 
 # Global variables
 TODAY_DATE = 19 # Update this with every new day of month of March
+NUM_TWEETS = 100
 
 # Read through list of athletes
 with open("athletes.json", "r") as f:
     athlete_dictionary = json.loads(f.read())
-
-dump = open("data_dump.txt", "w+")
 
 # For every athlete...
 for key in athlete_dictionary:
@@ -28,14 +27,17 @@ for key in athlete_dictionary:
         file = open(file_name, "w+")
 
         # Search tweets within each day
-        tweets = api.search(q=athlete_dictionary[key], since=start_str, until=until_str, tweet_mode="extended")
+        tweets = api.search(q=athlete_dictionary[key], count=NUM_TWEETS, since=start_str, until=until_str, tweet_mode="extended")
 
         # Get full text for both regular and retweeted tweets
         for tweet in tweets:
             if "retweeted_status" in dir(tweet):
                 tweet = tweet.retweeted_status.full_text
             else:
-                tweet=tweet.full_text
+                tweet = tweet.full_text
+
+            if "https" in tweet:
+                continue
 
             # Open new file and write
             file.write(tweet.encode('utf-8')+"\n\n")
