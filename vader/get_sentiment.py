f528c5e394235ddd83382c70188a4d821fd91a6f
Nick Marcopoli <nick@marcopo.li>
2020-04-23 15:59:53 -0400

    added vader

---
+++ vader/get_sentiment.py
@@ -0,0 +1,19 @@
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from nltk import download
+from sys import stdin
+
+def sentiment_scores(sentence): 
+    sid_obj = SentimentIntensityAnalyzer()
+    sentiment_dict = sid_obj.polarity_scores(sentence)
+    score = sentiment_dict['compound'] # will be a score between -1 and +1
+    # convert score to the same scale as the original regression model
+    score *= 2
+    score += 2
+    # now, score will be between 0 and +4, just like the original one
+    return score
+
+if __name__ == "__main__":
+    if not download('vader_lexicon', download_dir='./'):
+        raise IOError("Vader lexicon not downloaded.")
+    for line in stdin:
+        print(sentiment_scores(line.strip()))
